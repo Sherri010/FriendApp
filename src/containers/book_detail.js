8bfdc24468a3458a2e29c5d91fa8a886dc0cbe2e
sherri010 <aminshahidi.sh@gmail.com>
2017-03-09 06:46:27 -0800

    shows selected book details

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -1,18 +1,25 @@
 
 import React, {Component} from 'react';
-import { connet } from 'react-redux';
+import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render(){
-    return (
-      <div> Book Detail container </div>
+
+      if(!this.props.book){
+        return <div> Select a book to show </div>
+      }
+      
+    return (  <div>
+        <h3> Details for: </h3>
+         <div> {this.props.book.title} </div>
+     </div>
     );
   }
 }
 
 function mapStateToProps(state){
   return {
-    book:state.activeBook;
+    book:state.activeBook
   };
 }
 export default connect(mapStateToProps)(BookDetail)
